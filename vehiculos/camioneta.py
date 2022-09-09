28c3c2ccb061c7f000f0d6bee0190861bea353ba
Albertojserr <alberto.j.serrano@gmail.com>
2022-09-09 18:59:41 +0200

    entrega

---
+++ vehiculos/camioneta.py
@@ -0,0 +1,7 @@
+from vehiculos.coche import Coche
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        Coche.__init__(self, color, ruedas, velocidad, cilindrada)
+        self.carga=carga
+    def __str__(self):
+        return Coche.__str__(self) + ", {} kg".format(self.carga)
